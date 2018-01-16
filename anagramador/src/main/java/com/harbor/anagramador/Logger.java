3cb52374ab8b5239625314459bee1dfc109f36aa
Jimmy Porto <ireturnerd@gmail.com>
2018-01-16 10:48:39 -0200

    Logger updated

--- anagramador/src/main/java/com/harbor/anagramador/Logger.java
+++ anagramador/src/main/java/com/harbor/anagramador/Logger.java
@@ -7,18 +7,55 @@ package com.harbor.anagramador;
 public class Logger {
 
 	private Boolean doLog;
+	private Long[] times = new Long[2];
+	private Boolean counter = false;
 
 	public Logger(Boolean doLog) {
 		super();
 		this.doLog = doLog;
 	}
-	
-	public void show(String... values){
-		if(doLog) {
+
+	/**
+	 * This methods print in console the log that the user want to print.
+	 * 
+	 * @param values
+	 */
+	public void show(String... values) {
+		if (doLog) {
 			for (String value : values) {
 				System.err.println(value);
 			}
 		}
+	}
+
+	/**
+	 * This method add a time of now.
+	 * If the times already have two values, the method will override the next position (0,1) like a cycle.
+	 * 
+	 * @param timeInMillis
+	 */
+	public void time() {
+		times[counter ? 0 : 1] = System.currentTimeMillis();
+		counter = !counter;
+	}
+	
+	/**
+	 * This method add a time that the user want to save.
+	 * If the times already have two values, the method will override the next position (0,1) like a cycle.
+	 * 
+	 * @param timeInMillis
+	 */
+	public void time(Long timeInMillis) {
+		times[counter ? 0 : 1] = timeInMillis;
+		counter = !counter;
+	}
+
+	public void showTime() {
+		String log = "No time to show";
+		if (times.length == 2) {
+			log = "Time spent: " + (times[1] - times[0]) + "milliseconds.";
+		}
+		show(log);
 	}
 
 }
